1be482417bb1b2546d5820bf35f1586dd52383fe
Huzaifa <daxing.huzaifa123@gmail.com>
2023-01-04 22:24:52 +0500

    first commit

---
+++ home.js
@@ -0,0 +1,180 @@
+console.log("Working");
+showNotes();
+// Recognizing variables
+
+let addBtn = document.getElementById('addBtn');
+let clearBtn = document.getElementById('clearBtn');
+let addTxt = document.getElementById('addTxt');
+let deleteBtn = document.getElementById('btn-primary');
+let plus = document.getElementById('plus');
+let form = document.getElementById('form');
+let notesBtn = document.getElementById('notesBtn');
+
+const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+// Main Working
+
+
+
+function removeError() {
+    setTimeout(() => {
+        let nullval = document.getElementById('error');
+        nullval.classList.add('none');
+        nullval.classList.remove('block');
+        // console.log(nullval);
+        plus.classList.remove('none');
+        // console.log("set timeout-1");
+    }, 1500);
+}
+
+function addError() {
+    // console.log("null val");
+    let nullval = document.getElementById('error');
+    nullval.classList.add('block');
+    nullval.classList.remove('none');
+    plus.classList.add('none');
+}
+
+let noteTitle = document.getElementById('noteTitle');
+
+noteTitle.addEventListener('input', (e) => {
+    let titleVal = e.target.value;
+    // console.log(titleVal);
+    if (titleVal === "") {
+        e.preventDefault();
+        addError();
+        removeError();
+        plus.classList.add('hidden');
+    }
+    else {
+        plus.classList.remove('hidden');
+    }
+})
+
+
+plus.addEventListener('click', () => {
+    form.classList.remove('none');
+    notesBtn.classList.remove('none')
+    addBtn.classList.remove('none')
+})
+
+
+
+// Working on if user add a note
+
+
+addBtn.addEventListener('click', (e) => {
+    let noteTitle = document.getElementById('noteTitle');
+    e.preventDefault();
+    let title = noteTitle.value,
+        noteContent = addTxt.value;
+    if (title || noteContent != "") {
+        let noteObj = {
+            Title: title, noteContent: noteContent
+        }
+
+        notes.push(noteObj);
+        localStorage.setItem("notes", JSON.stringify(notes))
+        noteTitle.value = "";
+        addTxt.value = "";
+        plus.classList.add('none')
+        console.log(notes);
+        showNotes();
+        window.location.reload();
+    }
+})
+
+
+
+// Working on if user want to clear all notes
+
+clearBtn.addEventListener('click', () => {
+    localStorage.removeItem("notes");
+    window.location.reload();
+})
+
+// Working on showing notes
+function showNotes() {
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+    // first removing all the notes then adding new notes
+    document.querySelectorAll('.noteCard').forEach(new_2 => new_2.remove());
+    notes.forEach((note, index) => {
+        let html = `
+        <div class="noteCard p-2 mx-my new_2" id="noteContainer">
+        <div class="">
+            <h5 class="card-title color-white ml">${note.Title}</h5>
+        </div>
+        <div class="" id="noteContent">
+            <p class="card-text">${note.noteContent}</p>
+            <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary mt-1">Delete
+                Note</button>
+            <button id="" class="btn btn-primary mt-1 ml-1">Edit Note</button>
+        </div>
+    </div> `
+
+        let addNote = document.getElementById('notes')
+        if (notes.length != 0) {
+            let form = document.getElementById('form');
+            // addNote.innerHTML = html
+            addNote.insertAdjacentHTML("afterend", html)
+            form.classList.add('none')
+        }
+
+        if (notes.length >= 3) {
+            
+            let addBtn = document.getElementById('addBtn');
+            let notesBtn = document.getElementById('notesBtn');
+            notesBtn.classList.remove('none')
+            addBtn.classList.add('none')
+        }
+    })
+}
+
+
+function deleteNote(index) {
+
+    console.log("i am deleting this", index);
+    let notes = localStorage.getItem('notes')
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+}
+
+// Search Bar
+
+let search = document.getElementById('searchTxt');
+search.addEventListener("input", function () {
+
+    let inputVal = search.value.toLowerCase();
+    // console.log('Input event fired!', inputVal);
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function (element) {
+        let cardTxt = element.getElementsByTagName("p")[0].innerText;
+        if (cardTxt.includes(inputVal)) {
+            element.style.display = "block";
+        }
+        else {
+            element.style.display = "none";
+        }
+        // console.log(cardTxt);
+    })
+})
+
+console.log(notes);
+
+
+hamb.addEventListener('click', ()=>{
+    cross.classList.remove('none')
+    options.classList.remove('none')
+    hamb.classList.add('none')
+})
+cross.addEventListener('click', ()=>{
+    cross.classList.add('none')
+    options.classList.add('none')
+    hamb.classList.remove('none')
+})
---
+++ script.js
@@ -0,0 +1,250 @@
+document.querySelector('.img__btn').addEventListener('click', function () {
+    document.querySelector('.cont').classList.toggle('s--signup');
+});
+
+let username = document.getElementById('name');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let signUp = document.getElementById('signUpBtn');
+let nameSpan = document.getElementById('nameSpan');
+let emailSpan = document.getElementById('emailSpan');
+let loginbtn = document.getElementById('login');
+let loginEmail = document.getElementById('loginEmail');
+let loginPass = document.getElementById('loginPass');
+let hide = document.getElementById('hide');
+let show = document.getElementById('show');
+let loginHide = document.getElementById('loginHide');
+let loginShow = document.getElementById('loginShow');
+let go_to_signup = document.getElementById('go_to_signup');
+let go_to_login = document.getElementById('go_to_login');
+let login_pg = document.getElementById('login_pg');
+let sign_in_pg = document.getElementById('sign_in_pg');
+
+function removeError(txt, area2) {
+    setTimeout(() => {
+        area2.innerText = txt, area2.style.color = '#cfcfcf'
+    }, 1500);
+}
+
+function addError(span, area) {
+    area.innerText = `*Enter your ${span}*`, area.style.color = '#FF0000'
+}
+
+
+//------------- Working on sign up -------------//
+
+
+signUp.addEventListener('click', (e) => {
+
+    let data = class {
+        userData(username, email, password) {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+        }
+    };
+
+    let nameVal = username.value
+    let emailVal = email.value
+    let passVal = password.value
+    // console.log(nameVal);
+    // console.log(emailVal);
+    // console.log(passVal);
+
+    if (nameVal == "" && emailVal == "" && passVal == "") {
+        console.log("all null");
+        nameSpan.innerText = `*Enter all the requirements*`, nameSpan.style.color = '#FF0000'
+        removeError("name", nameSpan);
+        // console.log(removeError());
+
+    }
+
+    else if (nameVal == "") {
+        email.value = ""
+        password.value = ""
+        addError("name", nameSpan);
+        removeError("name", nameSpan);
+        // console.log(nameVal);
+    }
+    else if (emailVal == "") {
+        username.value = ""
+        password.value = ""
+        addError("email", nameSpan);
+        removeError("name", nameSpan);
+        // console.log(emailVal); 
+    }
+    else if (passVal == "") {
+        username.value = ""
+        email.value = ""
+        addError("password", nameSpan);
+        removeError("name", nameSpan);
+        // console.log(name.value);
+        // console.log(email.value);
+        // console.log(password.value); 
+    }
+    else {
+        console.log("all sett");
+
+        const userData = JSON.parse(localStorage.getItem("userData") || "[]");
+
+        if (userData == null) {
+            userData = []
+        }
+        console.log("userData", userData);
+        
+        
+        let user = new data()
+        user.userData(nameVal, emailVal, passVal)
+        console.log(user);
+        userData.push(user);
+        let set = localStorage.setItem("userData", JSON.stringify(userData))
+        console.log(set);
+        username.value = ""
+        email.value = ""
+        password.value = ""
+        window.location.reload();
+    }
+})
+
+//------------- Working on Login -------------//
+
+
+
+loginbtn.addEventListener('click', () => {
+    let emailVal = loginEmail.value
+    let passVal = loginPass.value
+
+    // console.log(`Email is`, emailVal, `and the password is`, passVal );`
+
+    if (emailVal == "" && passVal == "") {
+        // console.log("all null");
+        emailSpan.innerText = `*Enter all the requirements*`, emailSpan.style.color = '#FF0000'
+        removeError("email", emailSpan)
+
+    }
+
+    else if (emailVal == "") {
+        loginEmail.value = ""
+        loginPass.value = ""
+        addError("email", emailSpan);
+        removeError("email", emailSpan)
+        // console.log("null email"); 
+    }
+    else if (passVal == "") {
+
+        loginEmail.value = ""
+        loginPass.value = ""
+        addError("password", emailSpan);
+        removeError("email", emailSpan)
+        // console.log("null pass"); 
+    }
+
+    else {
+        // console.log(emailVal);
+        // console.log("all sett");
+        const userData = JSON.parse(localStorage.getItem("userData") || "[]");
+        // console.log(userData);
+        userData.forEach(arrIndex => {
+            let email = arrIndex.email
+            let password = arrIndex.password
+            if (email == emailVal && password == passVal) {
+                console.log("ok");
+                loginEmail.value = ""
+                loginPass.value = ""
+                window.location.href = "home.html"
+            }
+            else if (email !== emailVal && password !== passVal){
+                console.log("wrong");
+                loginEmail.classList.add('border')
+                loginPass.classList.add('border')
+                emailSpan.innerText = `*Enter all the requirements*`, emailSpan.style.color = '#FF0000'
+                loginEmail.value = ""
+                loginPass.value = ""
+                remove();
+            }
+            else if (email !== emailVal){
+                loginEmail.value = ""
+                // loginPass.value = ""
+                emailSpan.innerText = `*Email is incorrect*`, emailSpan.style.color = '#FF0000'
+                removeError("Email", emailSpan)
+                console.log("only email is incorrect");
+            }
+            else if (password !== passVal){
+                loginPass.value = ""
+                emailSpan.innerText = `*Password is incorrect*`, emailSpan.style.color = '#FF0000'
+                removeError("Email", emailSpan)
+                console.log("only Password is incorrect");
+            }
+        });
+        
+        
+        function remove() {
+            setInterval(() => {
+                loginEmail.classList.remove('border')
+                loginPass.classList.remove('border')
+                emailSpan.innerText = 'Email', emailSpan.style.color = '#cfcfcf'
+            }, 5500);
+        }
+        
+        // let email = userData.email
+        // console.log(typeof userData);
+        // if(emailVal = )
+
+        // const userData = JSON.parse(localStorage.getItem("userData") || "[]");
+
+        // if (userData == null) {
+        //     userData = []
+        // }
+        // console.log("userData", userData);
+
+
+        // let user = new data()
+        // user.userData(nameVal, emailVal, passVal)
+        // console.log(user);
+        // userData.push(user);
+        // localStorage.setItem("userData", JSON.stringify(userData))
+        // username.value = ""
+        // email.value = ""
+        // password.value = ""
+        // window.location.reload();
+    }
+})
+
+// For signup showing password icon
+
+
+hide.addEventListener('click', () => {
+    hide.style.display = 'none'
+    show.style.display = 'block'
+    password.setAttribute('type', "text")
+})
+show.addEventListener('click', () => {
+    show.style.display = 'none'
+    hide.style.display = 'block'
+    password.setAttribute('type', "password")
+})
+
+// For Login showing password icon
+
+loginHide.addEventListener('click', () => {
+    loginHide.style.display = 'none'
+    loginShow.style.display = 'block'
+    loginPass.setAttribute('type', "text")
+})
+loginShow.addEventListener('click', () => {
+    loginShow.style.display = 'none'
+    loginHide.style.display = 'block'
+    loginPass.setAttribute('type', "password")
+})
+
+
+go_to_signup.addEventListener('click', ()=>{
+    login_pg.classList.add('transform1');
+    sign_in_pg.classList.add('transform2');
+})
+
+
+go_to_login.addEventListener('click', ()=>{
+    login_pg.classList.remove('transform1');
+    sign_in_pg.classList.remove('transform2');
+})
